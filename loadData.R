7c2efc15119af5e5f2ee88238879a547b2ed0ae1
James Myers <james.myers@york.ac.uk>
2025-07-11 14:19:40 +0100

    loadData.R update

--- loadData.R
+++ loadData.R
@@ -1,7 +1,7 @@
 #### Load data ####
 
 # 1. Set the source folder path
-source_folder <- 'G:/Shared drives/Curated Dataset/data/source' # IMPORTANT: Change this to your actual folder path!
+source_folder <- 'G:/Shared drives/Curated Dataset/data/source'
 
 # 2. Get a list of files
 file_list <- list.files(path = source_folder,
