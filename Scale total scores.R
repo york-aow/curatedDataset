b8cc92d5ee85fe9d110a8c7fba720eb975df8a90
James Tait <james.tait@york.ac.uk>
2025-08-15 16:11:43 +0100

    SDQ scale scores done

--- Scale total scores.R
+++ Scale total scores.R
@@ -1,4 +1,4 @@
-install.packages("psych")
+#install.packages("psych")
 
 library(psych)
 
@@ -30,7 +30,7 @@ recode02_20 <- function(x) {
 #### EDE-QS ####
 
 # Create list of items
-EDEQS_items <-
+edeqs_items <-
   c("awb2_12_eat_hbt_1_a5",
     "awb2_12_eat_hbt_2_a5",
     "awb2_12_eat_hbt_3_a5",
@@ -73,11 +73,11 @@ summary(both_mods_24_x$awb2_12_wght_2_a5)
 
 # Recode item responses from 1-4 to 0-3
 both_mods_24_x <- both_mods_24_x %>%
-  mutate(across(all_of(EDEQS_items), recode14_03))
+  mutate(across(all_of(edeqs_items), recode14_03))
 
 # Check new item response frequencies and NA's against old (above)
-for(item in both_mods_24_x[EDEQS_items]){print(summary(item))}
-for(item in both_mods_24_x[EDEQS_items]){print(table(item))}
+for(item in both_mods_24_x[edeqs_items]){print(summary(item))}
+for(item in both_mods_24_x[edeqs_items]){print(table(item))}
 
 # How many responded 0-days to Q9 (and therefore skipped Q10)?
 table(as_factor(both_mods_24_x$awb2_12_eat_hbt_9_a5))
@@ -99,34 +99,34 @@ table(as.factor(both_mods_24_x$awb2_12_eat_hbt_9_a5),
 
 # Identify complete EDE-QS responses
 both_mods_24_x <- both_mods_24_x %>%
-  mutate(EDEQS_complete = rowSums(is.na(select(., !!!EDEQS_items))) == 0) # !!! removes quote marks from the list items
+  mutate(edeqs_complete = rowSums(is.na(select(., !!!edeqs_items))) == 0) # !!! removes quote marks from the list items
 
-table(both_mods_24_x$EDEQS_complete)
+table(both_mods_24_x$edeqs_complete)
 6411+1144 # 7555
 6411/7555 # 84.9% complete / 15% missing
 
-# Compute total scores for those with complete EDEQS, and assign NA to those without complete EDEQS
+# Compute total scores for those with complete EDE-QS, and assign NA to those without complete edeqs
 both_mods_24_x <- both_mods_24_x %>%
-  mutate(EDEQS_total = ifelse(EDEQS_complete, rowSums(select(., !!!EDEQS_items), na.rm = TRUE), NA))
+  mutate(edeqs_total = ifelse(edeqs_complete, rowSums(select(., !!!edeqs_items), na.rm = TRUE), NA))
 
 # Sense check
-summary(both_mods_24_x$EDEQS_total)
-table(as_factor(both_mods_24_x$EDEQS_total))
-table(as_factor(both_mods_24_x$EDEQS_total), both_mods_24_x$EDEQS_complete)
+summary(both_mods_24_x$edeqs_total)
+table(as_factor(both_mods_24_x$edeqs_total))
+table(as_factor(both_mods_24_x$edeqs_total), both_mods_24_x$edeqs_complete)
 
 # Create a variable that counts the number of NAs across the items for each participant
 both_mods_24_x <- both_mods_24_x %>%
-  mutate(EDEQS_nas = rowSums(is.na(select(., !!!EDEQS_items))))
+  mutate(edeqs_nas = rowSums(is.na(select(., !!!edeqs_items))))
 
-table(as_factor(both_mods_24_x$EDEQS_nas))
+table(as_factor(both_mods_24_x$edeqs_nas))
 6411+1144#7555
-table(as_factor(both_mods_24_x$EDEQS_nas), as_factor(both_mods_24_x$EDEQS_complete))
+table(as_factor(both_mods_24_x$edeqs_nas), as_factor(both_mods_24_x$edeqs_complete))
 
 # Inter-item correlations
-cor(both_mods_24_x[EDEQS_items], use = "pairwise.complete.obs")
+cor(both_mods_24_x[edeqs_items], use = "pairwise.complete.obs")
 
 # Internal reliability
-alpha(both_mods_24_x[EDEQS_items])
+alpha(both_mods_24_x[edeqs_items])
 
 
 #### RCADS ####
@@ -262,7 +262,7 @@ both_mods_24_x <- both_mods_24_x %>%
 
 table(both_mods_24_x$swemwbs_complete)
 
-# Compute total scores for those with complete SWEMWBS, and assign NA to those without complete EDEQS
+# Compute total scores for those with complete SWEMWBS, and assign NA to those without
 both_mods_24_x <- both_mods_24_x %>%
   mutate(swemwbs_total = ifelse(swemwbs_complete, rowSums(select(., !!!swemwbs_items), na.rm = TRUE), NA))
 
@@ -313,31 +313,7 @@ sdq_pro_items <- c("awb2_1_sdq_1_a10",
                    "awb2_1_sdq_17_a10", 
                    "awb2_1_sdq_20_a10")
 
-sdq_items <- c("awb2_1_sdq_3_a10",
-               "awb2_1_sdq_8_a10",
-               "awb2_1_sdq_13_a10",
-               "awb2_1_sdq_16_a10",
-               "awb2_1_sdq_24_a10",
-               "awb2_1_sdq_5_a10",
-               "awb2_1_sdq_7_a10", # This item is reverse coded
-               "awb2_1_sdq_12_a10",
-               "awb2_1_sdq_18_a10",
-               "awb2_1_sdq_22_a10",
-               "awb2_1_sdq_2_a10",
-               "awb2_1_sdq_10_a10", 
-               "awb2_1_sdq_15_a10",
-               "awb2_1_sdq_21_a10", # This item is reverse coded
-               "awb2_1_sdq_25_a10", # This item is reverse coded
-               "awb2_1_sdq_6_a10",
-               "awb2_1_sdq_11_a10", # This item is reverse coded
-               "awb2_1_sdq_14_a10", # This item is reverse coded
-               "awb2_1_sdq_19_a10", 
-               "awb2_1_sdq_23_a10",
-               "awb2_1_sdq_1_a10",
-               "awb2_1_sdq_4_a10", 
-               "awb2_1_sdq_9_a10", 
-               "awb2_1_sdq_17_a10", 
-               "awb2_1_sdq_20_a10") 
+sdq_items <- c(sdq_emo_items, sdq_con_items, sdq_hyp_items, sdq_peer_items, sdq_pro_items)
 
 sdq_reverse_items <- c("awb2_1_sdq_7_a10", # This item is reverse coded
                        "awb2_1_sdq_21_a10", # This item is reverse coded
@@ -357,15 +333,80 @@ cor(both_mods_24_x[sdq_items], use = "pairwise.complete.obs")
 both_mods_24_x <- both_mods_24_x %>%
   mutate(across(all_of(sdq_reverse_items), recode02_20))
 
+# Identify complete subscale responses
+both_mods_24_x <- both_mods_24_x %>%
+  mutate(sdq_con_complete = rowSums(is.na(select(., !!!sdq_con_items))) == 0)
+
+both_mods_24_x <- both_mods_24_x %>%
+  mutate(sdq_emo_complete = rowSums(is.na(select(., !!!sdq_emo_items))) == 0)
+
+both_mods_24_x <- both_mods_24_x %>%
+  mutate(sdq_hyp_complete = rowSums(is.na(select(., !!!sdq_hyp_items))) == 0)
+
+both_mods_24_x <- both_mods_24_x %>%
+  mutate(sdq_peer_complete = rowSums(is.na(select(., !!!sdq_peer_items))) == 0)
+
+both_mods_24_x <- both_mods_24_x %>%
+  mutate(sdq_pro_complete = rowSums(is.na(select(., !!!sdq_pro_items))) == 0)
+
+table(both_mods_24_x$sdq_con_complete)
+table(both_mods_24_x$sdq_emo_complete)
+table(both_mods_24_x$sdq_hyp_complete)
+table(both_mods_24_x$sdq_peer_complete)
+table(both_mods_24_x$sdq_pro_complete)
 
 
+# Compute subscale total scores for those with complete subscale, and assign NA to those without
+both_mods_24_x <- both_mods_24_x %>%
+  mutate(sdq_con_total = ifelse(sdq_con_complete, rowSums(select(., !!!sdq_con_items), na.rm = TRUE), NA)) %>%
+  mutate(sdq_emo_total = ifelse(sdq_emo_complete, rowSums(select(., !!!sdq_emo_items), na.rm = TRUE), NA)) %>%
+  mutate(sdq_hyp_total = ifelse(sdq_hyp_complete, rowSums(select(., !!!sdq_hyp_items), na.rm = TRUE), NA)) %>%
+  mutate(sdq_peer_total = ifelse(sdq_peer_complete, rowSums(select(., !!!sdq_peer_items), na.rm = TRUE), NA)) %>%
+  mutate(sdq_pro_total = ifelse(sdq_pro_complete, rowSums(select(., !!!sdq_pro_items), na.rm = TRUE), NA))
 
+# Sense check
+summary(both_mods_24_x$sdq_con_total)
+table(as_factor(both_mods_24_x$sdq_con_total), both_mods_24_x$sdq_con_complete, useNA = "ifany")
 
+summary(both_mods_24_x$sdq_emo_total)
+table(as_factor(both_mods_24_x$sdq_emo_total), both_mods_24_x$sdq_emo_complete, useNA = "ifany")
 
+summary(both_mods_24_x$sdq_hyp_total)
+table(as_factor(both_mods_24_x$sdq_hyp_total), both_mods_24_x$sdq_hyp_complete, useNA = "ifany")
 
+summary(both_mods_24_x$sdq_peer_total)
+table(as_factor(both_mods_24_x$sdq_peer_total), both_mods_24_x$sdq_peer_complete, useNA = "ifany")
 
+summary(both_mods_24_x$sdq_pro_total)
+table(as_factor(both_mods_24_x$sdq_pro_total), both_mods_24_x$sdq_pro_complete, useNA = "ifany")
 
+# Compute internalising and externalising scores
 
+both_mods_24_x$sdq_ext_total <- 
+  ifelse(is.na(both_mods_24_x$sdq_con_total) | is.na(both_mods_24_x$sdq_hyp_total), NA, 
+         both_mods_24_x$sdq_con_total + both_mods_24_x$sdq_hyp_total)
+
+both_mods_24_x$sdq_int_total <- 
+  ifelse(is.na(both_mods_24_x$sdq_emo_total) | is.na(both_mods_24_x$sdq_peer_total), NA, 
+         both_mods_24_x$sdq_emo_total + both_mods_24_x$sdq_peer_total)
+
+summary(both_mods_24_x$sdq_ext_total)
+table(both_mods_24_x$sdq_con_complete, both_mods_24_x$sdq_ext_total)
+table(both_mods_24_x$sdq_hyp_complete, both_mods_24_x$sdq_ext_total)
+
+summary(both_mods_24_x$sdq_int_total)
+table(both_mods_24_x$sdq_emo_complete, both_mods_24_x$sdq_int_total)
+table(both_mods_24_x$sdq_peer_complete, both_mods_24_x$sdq_int_total)
+
+# Compute total difficulties 
+
+both_mods_24_x$sdq_dif_total <- 
+  ifelse(is.na(both_mods_24_x$sdq_ext_total) | is.na(both_mods_24_x$sdq_int_total), NA, 
+         both_mods_24_x$sdq_ext_total + both_mods_24_x$sdq_int_total)
+
+summary(both_mods_24_x$sdq_dif_total)
+table(both_mods_24_x$sdq_int_total, both_mods_24_x$sdq_dif_total, useNA = "ifany")
+table(both_mods_24_x$sdq_ext_total, both_mods_24_x$sdq_dif_total, useNA = "ifany")
 
 
 #### END ####
