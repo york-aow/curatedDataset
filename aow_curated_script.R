1b29ab73e75ab762d1b69d6c589ca5caed73a341
James Tait <james.tait@york.ac.uk>
2025-08-27 15:32:03 +0100

    Added compute scale total scores

--- aow_curated_script.R
+++ aow_curated_script.R
@@ -1,6 +1,13 @@
 # 0. Install and load required packages ####
-library(tidyverse)
-library(haven)
+
+install.packages("tidyverse") # tidy R packages for data manipulation etc
+install.packages("haven") # Import data of various formats
+install.packages("psych") # Function to pro-rate missing items (item.scores)
+
+library(tidyverse) # tidy R packages for data manipulation etc
+library(haven) # Import data of various formats
+library(psych) # Function to pro-rate missing items (item.scores)
+
 
 # 1. Load raw AoW data ####
 
@@ -19,8 +26,9 @@ for (file_path in file_list) {
   assign(object_name, readRDS(file_path))
 }
 
+# 2. Join and filter data ####
 
-# Rename age-in-months variables ####
+## Rename age-in-months variables ####
 # ...to distinguish between age-in-months variables from different dataframes.
 # This is important because data may not all have been collected at the same time.
 
@@ -43,13 +51,13 @@ bioimpedance_dr24 <- bioimpedance_dr24 %>%
   rename(age_m_bioimpedance = age_m) # (new_variable_name = old_variable_variable)
 
 
-# Select participants who completed both survey modules 231 and 232 ####
+## Select participants who completed both survey modules 231 and 232 ####
 aow_curated <- recruitment_dr24 %>% 
   filter(as_factor(has_survey_m231) == "Yes",
          as_factor(has_survey_m232) == "Yes")
 
 
-# Add data from survey modules ####
+## Add data from survey modules ####
 
 # Rename survey_version and survey_mode variables to distinguish between modules
 survey_mod231_main_dr24 <- survey_mod231_main_dr24 %>%
@@ -81,11 +89,11 @@ aow_curated <- aow_curated %>%
     by = "aow_recruitment_id"
   )
 
-# Filter data to only include 23-24 data (removing 22-23 data) ####
+## Filter data to only include 23-24 data (removing 22-23 data) ####
 aow_curated <- aow_curated %>% 
   filter(recruitment_era == '2023-24')
 
-# Exclude participants with age (months) difference between survey modules ####
+## Exclude participants with age (months) difference between survey modules ####
 # Compute difference in age (months) between survey modules
 aow_curated$survey_age_diff <- (aow_curated$age_survey232_m - aow_curated$age_survey231_m)
 
@@ -94,7 +102,7 @@ aow_curated <- aow_curated %>%
   filter(survey_age_diff == '0')
 
 
-#### Identify (and remove) duplicates ####
+## Identify (and remove) duplicates ####
 #implement rule for duplicated patient IDs (take data from earliest time point based on participant age in months)
 aow_curated <- aow_curated %>% 
   group_by(aow_person_id) %>% 
@@ -103,7 +111,7 @@ aow_curated <- aow_curated %>%
   ungroup()
 
 
-# Add height and weight data ####
+## Add height and weight data ####
 # Identify repeated variables
 repeated_vars1 <- intersect(names(aow_curated), names(heightweight_dr24)) %>%
   setdiff("aow_recruitment_id")
@@ -114,7 +122,7 @@ aow_curated <- aow_curated %>%
     by = "aow_recruitment_id")
 
 
-# Add skinfold data ####
+## Add skinfold data ####
 # Identify repeated variables
 repeated_vars1 <- intersect(names(aow_curated), names(skinfold_dr24)) %>%
   setdiff("aow_recruitment_id")
@@ -125,7 +133,7 @@ aow_curated <- aow_curated %>%
     by = "aow_recruitment_id")
 
 
-# Add bloodpressure data ####
+## Add bloodpressure data ####
 # Identify repeated variables
 repeated_vars1 <- intersect(names(aow_curated), names(bloodpressure_dr24)) %>%
   setdiff("aow_recruitment_id")
@@ -136,7 +144,7 @@ aow_curated <- aow_curated %>%
     by = "aow_recruitment_id")
 
 
-# Add bioimpedance data ####
+## Add bioimpedance data ####
 # Identify repeated variables
 repeated_vars1 <- intersect(names(aow_curated), names(bioimpedance_dr24)) %>%
   setdiff("aow_recruitment_id")
@@ -146,8 +154,361 @@ aow_curated <- aow_curated %>%
     bioimpedance_dr24 %>% select(-any_of(repeated_vars1)),
     by = "aow_recruitment_id")
 
+# Note: Data were not all collected at the same time, see differences in age (months)
+# between measurements
+table(aow_curated$age_survey231_m, aow_curated$age_m_heightweight)
+table(aow_curated$age_survey231_m, aow_curated$age_m_skinfold)
+table(aow_curated$age_survey231_m, aow_curated$age_m_bloodpressure)
+table(aow_curated$age_survey231_m, aow_curated$age_m_bioimpedance)
 
 
+# 3. Compute scale total scores ####
+
+## Create recode functions ####
+
+# Recode Likert scale 1-4 to 0-3
+recode14_03 <- function(x) {
+  case_match(x,
+             1 ~ 0,
+             2 ~ 1,
+             3 ~ 2,
+             4 ~ 3,
+             NA ~ NA
+  )
+}
+
+# Reverse code 0-2 Likert scale
+recode02_20 <- function(x) {
+  case_match(x,
+             0 ~ 2,
+             1 ~ 1,
+             2 ~ 0,
+             NA ~ NA
+  )
+}
+
+# Reverse code 1-5 Likert scale
+recode15_51 <- function(x) {
+  case_match(x,
+             1 ~ 5,
+             2 ~ 4,
+             3 ~ 3,
+             4 ~ 2,
+             5 ~ 1,
+             NA ~ NA
+  )
+}
 
 
+## EDE-QS ####
 
+# Create list of items
+edeqs_items <-
+  c("awb2_12_eat_hbt_1_a5",
+    "awb2_12_eat_hbt_2_a5",
+    "awb2_12_eat_hbt_3_a5",
+    "awb2_12_eat_hbt_4_a5",
+    "awb2_12_eat_hbt_5_a5",
+    "awb2_12_eat_hbt_6_a5",
+    "awb2_12_eat_hbt_7_a5",
+    "awb2_12_eat_hbt_8_a5",
+    "awb2_12_eat_hbt_9_a5",
+    "awb2_12_eat_hbt_10_a5",
+    "awb2_12_wght_1_a5",
+    "awb2_12_wght_2_a5")
+
+# Recode item responses from 1-4 to 0-3
+aow_curated <- aow_curated %>%
+  mutate(across(all_of(edeqs_items), recode14_03))
+
+# For those that responded 0 for item 9, impute 0 for item 10
+
+aow_curated <- aow_curated %>%
+  mutate(awb2_12_eat_hbt_10_a5 = ifelse(awb2_12_eat_hbt_9_a5 == 0, 0, awb2_12_eat_hbt_10_a5))
+
+# Identify complete EDE-QS responses
+aow_curated <- aow_curated %>%
+  mutate(edeqs_complete = rowSums(is.na(select(., !!!edeqs_items))) == 0) # !!! removes quote marks from the list items
+
+# Compute total scores for those with complete EDE-QS, and assign NA to those without complete edeqs
+aow_curated <- aow_curated %>%
+  mutate(edeqs_total = ifelse(edeqs_complete, rowSums(select(., !!!edeqs_items), na.rm = TRUE), NA))
+
+## RCADS ####
+
+# Create list of depression items
+rcads_dep_items <- c("awb2_1_illhealth_1",
+                     "awb2_1_illhealth_4",
+                     "awb2_1_illhealth_8",
+                     "awb2_1_illhealth_10",
+                     "awb2_1_illhealth_13",
+                     "awb2_1_illhealth_15",
+                     "awb2_1_illhealth_16",
+                     "awb2_1_illhealth_19",
+                     "awb2_1_illhealth_21",
+                     "awb2_1_illhealth_24")
+
+# Create list of anxiety items
+rcads_anx_items <- c("awb2_1_illhealth_2",
+                     "awb2_1_illhealth_3",
+                     "awb2_1_illhealth_5",
+                     "awb2_1_illhealth_6",
+                     "awb2_1_illhealth_7",
+                     "awb2_1_illhealth_9",
+                     "awb2_1_illhealth_11",
+                     "awb2_1_illhealth_12",
+                     "awb2_1_illhealth_14",
+                     "awb2_1_illhealth_17",
+                     "awb2_1_illhealth_18",
+                     "awb2_1_illhealth_20",
+                     "awb2_1_illhealth_22",
+                     "awb2_1_illhealth_23",
+                     "awb2_1_illhealth_25")
+
+# Create list of all RCADS items
+rcads_items <- c(rcads_anx_items, rcads_dep_items)
+
+# Recode Likert scale from 1-4 to 0-3
+aow_curated <- aow_curated %>%
+  mutate(across(all_of(rcads_anx_items), recode14_03))
+
+aow_curated <- aow_curated %>%
+  mutate(across(all_of(rcads_dep_items), recode14_03))
+
+# Identify complete responses
+aow_curated <- aow_curated %>%
+  mutate(rcads_dep_complete = rowSums(is.na(select(., !!!rcads_dep_items))) == 0) # !!! removes quote marks from the list items
+
+aow_curated <- aow_curated %>%
+  mutate(rcads_anx_complete = rowSums(is.na(select(., !!!rcads_anx_items))) == 0) # !!! removes quote marks from the list items
+
+# Compute total scores for those with complete RCADS subscale, and assign NA to those with incomplete
+aow_curated <- aow_curated %>%
+  mutate(rcads_dep_total = ifelse(rcads_dep_complete, rowSums(select(., !!!rcads_dep_items), na.rm = TRUE), NA))
+
+aow_curated <- aow_curated %>%
+  mutate(rcads_anx_total = ifelse(rcads_anx_complete, rowSums(select(., !!!rcads_anx_items), na.rm = TRUE), NA))
+
+# Create RCADS scale total score (sum depression and anxiety, leave NA for any missing either subscale)
+aow_curated$rcads_total <- 
+  ifelse(is.na(aow_curated$rcads_anx_total) | is.na(aow_curated$rcads_dep_total), NA, 
+         aow_curated$rcads_anx_total + aow_curated$rcads_dep_total)
+
+## SWEMWBS ####
+
+# Create a list of SWEMWBS items
+swemwbs_items <- c("awb2_2_optmstc_1_a4",
+                   "awb2_2_useful_2_a4",
+                   "awb2_2_relxed_3_a4",
+                   "awb2_2_problems_4_a4",
+                   "awb2_2_think_clr_5_a4",
+                   "awb2_2_close_othrs_6_a4",
+                   "awb2_2_own_mnd_7_a4")
+
+# Identify complete SWEMWBS responses
+aow_curated <- aow_curated %>%
+  mutate(swemwbs_complete = rowSums(is.na(select(., !!!swemwbs_items))) == 0)
+
+# Compute total scores for those with complete SWEMWBS, and assign NA to those without
+aow_curated <- aow_curated %>%
+  mutate(swemwbs_total = ifelse(swemwbs_complete, rowSums(select(., !!!swemwbs_items), na.rm = TRUE), NA))
+
+
+## SDQ ####
+
+# List items
+
+sdq_emo_items <- c("awb2_1_sdq_3_a10",
+                   "awb2_1_sdq_8_a10",
+                   "awb2_1_sdq_13_a10",
+                   "awb2_1_sdq_16_a10",
+                   "awb2_1_sdq_24_a10")
+
+sdq_con_items <- c("awb2_1_sdq_5_a10",
+                   "awb2_1_sdq_7_a10", # This item is reverse coded
+                   "awb2_1_sdq_12_a10",
+                   "awb2_1_sdq_18_a10",
+                   "awb2_1_sdq_22_a10")
+
+sdq_hyp_items <- c("awb2_1_sdq_2_a10",
+                   "awb2_1_sdq_10_a10", 
+                   "awb2_1_sdq_15_a10",
+                   "awb2_1_sdq_21_a10", # This item is reverse coded
+                   "awb2_1_sdq_25_a10") # This item is reverse coded
+
+sdq_peer_items <- c("awb2_1_sdq_6_a10",
+                    "awb2_1_sdq_11_a10", # This item is reverse coded
+                    "awb2_1_sdq_14_a10", # This item is reverse coded
+                    "awb2_1_sdq_19_a10", 
+                    "awb2_1_sdq_23_a10")
+
+sdq_pro_items <- c("awb2_1_sdq_1_a10",
+                   "awb2_1_sdq_4_a10", 
+                   "awb2_1_sdq_9_a10", 
+                   "awb2_1_sdq_17_a10", 
+                   "awb2_1_sdq_20_a10")
+
+sdq_int_items <- c(sdq_emo_items, sdq_peer_items)
+
+sdq_ext_items <- c(sdq_con_items, sdq_hyp_items) 
+
+sdq_dif_items <- c(sdq_int_items, sdq_ext_items)
+
+sdq_items <- c(sdq_dif_items, sdq_pro_items)
+
+sdq_reverse_items <- c("awb2_1_sdq_7_a10", # This item is reverse coded
+                       "awb2_1_sdq_21_a10", # This item is reverse coded
+                       "awb2_1_sdq_25_a10", # This item is reverse coded
+                       "awb2_1_sdq_11_a10", # This item is reverse coded
+                       "awb2_1_sdq_14_a10") # This item is reverse coded
+
+# Recode reverse scored item responses from 0-2 to 2-0
+aow_curated <- aow_curated %>%
+  mutate(across(all_of(sdq_reverse_items), recode02_20))
+
+# Identify complete subscale responses
+aow_curated <- aow_curated %>%
+  mutate(sdq_con_complete = rowSums(is.na(select(., !!!sdq_con_items))) == 0)
+
+aow_curated <- aow_curated %>%
+  mutate(sdq_emo_complete = rowSums(is.na(select(., !!!sdq_emo_items))) == 0)
+
+aow_curated <- aow_curated %>%
+  mutate(sdq_hyp_complete = rowSums(is.na(select(., !!!sdq_hyp_items))) == 0)
+
+aow_curated <- aow_curated %>%
+  mutate(sdq_peer_complete = rowSums(is.na(select(., !!!sdq_peer_items))) == 0)
+
+aow_curated <- aow_curated %>%
+  mutate(sdq_pro_complete = rowSums(is.na(select(., !!!sdq_pro_items))) == 0)
+
+# Compute subscale total scores for those with complete subscale, and assign NA to those without
+aow_curated <- aow_curated %>%
+  mutate(sdq_con_total = ifelse(sdq_con_complete, rowSums(select(., !!!sdq_con_items), na.rm = TRUE), NA)) %>%
+  mutate(sdq_emo_total = ifelse(sdq_emo_complete, rowSums(select(., !!!sdq_emo_items), na.rm = TRUE), NA)) %>%
+  mutate(sdq_hyp_total = ifelse(sdq_hyp_complete, rowSums(select(., !!!sdq_hyp_items), na.rm = TRUE), NA)) %>%
+  mutate(sdq_peer_total = ifelse(sdq_peer_complete, rowSums(select(., !!!sdq_peer_items), na.rm = TRUE), NA)) %>%
+  mutate(sdq_pro_total = ifelse(sdq_pro_complete, rowSums(select(., !!!sdq_pro_items), na.rm = TRUE), NA))
+
+# Compute internalising and externalising scores
+
+aow_curated$sdq_ext_total <- 
+  ifelse(is.na(aow_curated$sdq_con_total) | is.na(aow_curated$sdq_hyp_total), NA, 
+         aow_curated$sdq_con_total + aow_curated$sdq_hyp_total)
+
+aow_curated$sdq_int_total <- 
+  ifelse(is.na(aow_curated$sdq_emo_total) | is.na(aow_curated$sdq_peer_total), NA, 
+         aow_curated$sdq_emo_total + aow_curated$sdq_peer_total)
+
+# Compute total difficulties 
+
+aow_curated$sdq_dif_total <- 
+  ifelse(is.na(aow_curated$sdq_ext_total) | is.na(aow_curated$sdq_int_total), NA, 
+         aow_curated$sdq_ext_total + aow_curated$sdq_int_total)
+
+
+## UCLA-3 ####
+
+ucla3_items <- c("awb2_4_loneliness_1",
+                 "awb2_4_loneliness_2",
+                 "awb2_4_loneliness_3")
+
+# Identify complete UCLA-3 responses
+aow_curated <- aow_curated %>%
+  mutate(ucla3_complete = rowSums(is.na(select(., !!!ucla3_items))) == 0)
+
+# Compute total scores for those with complete UCLA-3, and assign NA to those without
+aow_curated <- aow_curated %>%
+  mutate(ucla3_total = ifelse(ucla3_complete, rowSums(select(., !!!ucla3_items), na.rm = TRUE), NA))
+
+
+## UCLA-4 ####
+
+ucla4_items <- c("awb2_4_loneliness_1",
+                 "awb2_4_loneliness_2",
+                 "awb2_4_loneliness_3",
+                 "awb2_4_loneliness_4")
+
+# Identify complete UCLA-4 responses
+aow_curated <- aow_curated %>%
+  mutate(ucla4_complete = rowSums(is.na(select(., !!!ucla4_items))) == 0)
+
+# Compute total scores for those with complete UCLA-4, and assign NA to those without
+aow_curated <- aow_curated %>%
+  mutate(ucla4_total = ifelse(ucla4_complete, rowSums(select(., !!!ucla4_items), na.rm = TRUE), NA))
+
+
+## BRS ####
+
+brs_items <- c("awb2_9_resil1_a5",
+               "awb2_9_resil2_a5", # This item is reverse coded
+               "awb2_9_resil3_a5",
+               "awb2_9_resil4_a5", # This item is reverse coded
+               "awb2_9_resil5_a5",
+               "awb2_9_resil6_a5") # This item is reverse coded
+
+brs_reverse_items <- c("awb2_9_resil2_a5", # This item is reverse coded
+                       "awb2_9_resil4_a5", # This item is reverse coded
+                       "awb2_9_resil6_a5") # This item is reverse coded
+
+# Recode reverse scored item responses from 0-2 to 2-0
+aow_curated <- aow_curated %>%
+  mutate(across(all_of(brs_reverse_items), recode15_51))
+
+# Identify complete BRS responses
+aow_curated <- aow_curated %>%
+  mutate(brs_complete = rowSums(is.na(select(., !!!brs_items))) == 0)
+
+# Compute total scores for those with complete BRS, and assign NA to those without
+aow_curated <- aow_curated %>%
+  mutate(brs_total = ifelse(brs_complete, rowSums(select(., !!!brs_items), na.rm = TRUE), NA))
+
+
+## YAP-S ####
+
+yaps_items <- c("awb4_2_outside_schl_1_r7",
+                "awb4_2_outside_schl_2_r7",
+                "awb4_2_outside_schl_3_r7",
+                "awb4_2_outside_schl_4_r7",
+                "awb4_2_overall_a5")
+
+# Identify complete YAP-S responses
+aow_curated <- aow_curated %>%
+  mutate(yaps_complete = rowSums(is.na(select(., !!!yaps_items))) == 0)
+
+# Compute total scores for those with complete YAP-S, and assign NA to those without
+aow_curated <- aow_curated %>%
+  mutate(yaps_total = ifelse(yaps_complete, rowSums(select(., !!!yaps_items), na.rm = TRUE), NA))
+
+
+## Summary ####
+
+summary(aow_curated$edeqs_total)
+summary(aow_curated$rcads_anx_total)
+summary(aow_curated$rcads_dep_total)
+summary(aow_curated$rcads_total)
+summary(aow_curated$swemwbs_total)
+summary(aow_curated$sdq_con_total)
+summary(aow_curated$sdq_emo_total)
+summary(aow_curated$sdq_hyp_total)
+summary(aow_curated$sdq_peer_total)
+summary(aow_curated$sdq_pro_total)
+summary(aow_curated$sdq_int_total)
+summary(aow_curated$sdq_ext_total)
+summary(aow_curated$sdq_dif_total)
+summary(aow_curated$ucla3_total)
+summary(aow_curated$ucla4_total)
+summary(aow_curated$brs_total)
+summary(aow_curated$yaps_total)
+
+
+# 4. Divide data into year groups ####
+
+aow_curated_y8 <- aow_curated %>% 
+  filter(year_group == 8)
+
+aow_curated_y9 <- aow_curated %>% 
+  filter(year_group == 9)
+
+aow_curated_y10 <- aow_curated %>% 
+  filter(year_group == 10)
